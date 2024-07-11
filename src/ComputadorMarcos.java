a6da1537003d6917cfaa3f7198d78c3ec3d4f0f5
MarcosWinther <winthermarcos@gmail.com>
2024-07-10 21:06:56 -0300

    feat: criado novas classes e aplicado os conceitos de herança e encapsulamento nos arquivos do projeto

--- src/ComputadorMarcos.java
+++ src/ComputadorMarcos.java
@@ -2,7 +2,18 @@
 public class ComputadorMarcos {
 	public static void main(String[] args) {
 		MSNMessenger msn = new MSNMessenger();
-		
+		System.out.println("MSN");
 		msn.enviarMessagem();
+		msn.receberMensagem();
+		
+		FacebookMessenger fcb = new FacebookMessenger();
+		System.out.println("Facebook");
+		fcb.enviarMessagem();
+		fcb.receberMensagem();
+		
+		Telegram tlg = new Telegram();
+		System.out.println("Telegram");
+		tlg.enviarMessagem();
+		tlg.receberMensagem();
 	}
 }
--- src/MSNMessenger.java
+++ src/MSNMessenger.java
@@ -1,21 +1,4 @@
 
-public class MSNMessenger {
-	public void enviarMessagem() {
-		validarConectadoInternet();
-		System.out.println("Enviando mensagem");
-		salvarHistoricoMensagem();
-	}
+public class MSNMessenger extends ServicoMensagemInstantanea {
 	
-	public void receberMensagem() {
-		System.out.println("Recebendo mensagem");
-		salvarHistoricoMensagem();
-	}
-	
-	private void validarConectadoInternet() {
-		System.out.println("Validando se está conectado a Internet");
-	}
-	
-	private void salvarHistoricoMensagem() {
-		System.out.println("Salvando o histórico de mensagem");
-	}
 }
